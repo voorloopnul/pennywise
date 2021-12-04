ac1ef481cbf2492af6e8eafe0db18260793ff2b5
Ricardo Pascal <voorloop@gmail.com>
2021-12-04 20:14:13 +0100

    make pennywise consume tools from toml file.

--- it/tools/q.py
+++
@@ -1,6 +0,0 @@
-from it.core.base import BaseTool
-
-
-class Quast(BaseTool):
-    image = "staphb/quast:latest"
-    prefix_cmd = "quast.py"
--- it/tools/s.py
+++
@@ -1,6 +0,0 @@
-from it.core.base import BaseTool
-
-
-class Spades(BaseTool):
-    image = "staphb/spades:latest"
-    prefix_cmd = "spades.py"
