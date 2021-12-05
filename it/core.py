3a928193b64904ab81ebdb4bcc0a90624195f8bb
Ricardo Pascal <voorloop@gmail.com>
2021-12-05 19:29:56 +0100

    multiple updates
     - add function to return config_file path.
     - refactor load_tools
     - add pennywise help text

--- it/core.py
+++ it/core.py
@@ -2,23 +2,26 @@ import requests
 from pathlib import Path
 
 
+def config_file() -> Path:
+    return Path.home() / ".pennywise" / "pennywise.toml"
+
+
 def tool_list_exists() -> bool:
-    home_dir = Path.home()
-    pennywise_tool_file = home_dir / ".pennywise" / "pennywise.toml"
-    return pennywise_tool_file.exists()
+    return config_file().exists()
 
 
 def load_tools(user: str, branch: str) -> None:
-    home_dir = Path.home()
-    pennywise_tool_file = home_dir / ".pennywise" / "pennywise.toml"
-    pennywise_tool_file.parent.mkdir(parents=True, exist_ok=True)
+    config_file().parent.mkdir(parents=True, exist_ok=True)
 
-    print(f"Loading tool list from {user}/pennywise")
     url = f"https://raw.githubusercontent.com/{user}/pennywise/{branch}/pennywise.toml"
     response = requests.get(url)
+
     if response.status_code == 404:
-        print("Fork or User not found!")
+        print(f"{user}/pennywise not found!")
+
     elif response.status_code == 200:
-        pennywise_tool_file.write_text(response.text)
+        print(f"Loading tool list from {user}/pennywise")
+        config_file().write_text(response.text)
+
     else:
         print(f"Failed to load source from {user}")
