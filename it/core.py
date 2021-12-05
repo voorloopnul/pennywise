9400f27f452631ac275dbcce10eb930a97cfcf32
Ricardo Pascal <voorloop@gmail.com>
2021-12-05 19:08:43 +0100

    handle invalid user for source cmd
     - add more debug messsages
     - handle install conditionals

--- it/core.py
+++ it/core.py
@@ -8,13 +8,17 @@ def tool_list_exists() -> bool:
     return pennywise_tool_file.exists()
 
 
-def load_tools(user: str = "voorloopnul", branch: str = "main") -> None:
+def load_tools(user: str, branch: str) -> None:
     home_dir = Path.home()
     pennywise_tool_file = home_dir / ".pennywise" / "pennywise.toml"
     pennywise_tool_file.parent.mkdir(parents=True, exist_ok=True)
 
-    print("Loading tool list from voorloopnul/pennywise")
+    print(f"Loading tool list from {user}/pennywise")
     url = f"https://raw.githubusercontent.com/{user}/pennywise/{branch}/pennywise.toml"
     response = requests.get(url)
-    pennywise_tool_file.write_text(response.text)
-
+    if response.status_code == 404:
+        print("Fork or User not found!")
+    elif response.status_code == 200:
+        pennywise_tool_file.write_text(response.text)
+    else:
+        print(f"Failed to load source from {user}")
